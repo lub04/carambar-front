68354fc414604e1f592b342673fe900695ef42de
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2025-06-29 19:13:59 +0200

    fix deploiement 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
