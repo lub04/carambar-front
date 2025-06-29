f99831334468c4423e515dd348ef6c7eb02969b2
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2025-06-29 19:08:06 +0200

    fix deploiement 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/carambar-front/",
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
